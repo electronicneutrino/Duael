193f41d69d2b423339a2d89b135aa55d73e06855
electronicneutrino <electronicneutrino@users.noreply.github.com>
2019-07-24 00:18:31 -0400

    removed some unused assets, added movement script

--- Assets/Scripts/PlayerControl2D.cs
+++ Assets/Scripts/PlayerControl2D.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl2D : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+{ // Start is called before the first frame update
+   void Start()
+   { 
+       //QualitySettings.vSyncCount = 0;
+       //Application.targetFrameRate = 10;
+   }
+   // Update is called once per frame
+   void Update()
+   {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       Vector2 position = transform.position;
+       position.x = position.x + 1f * horizontal * Time.deltaTime;
+       position.y = position.y + 1f * vertical * Time.deltaTime;
+    //    position.Normalize();
+       transform.position = position;
+   }
 }
